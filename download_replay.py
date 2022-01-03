ba75cf92d770b64ce998037a0a4948338ea48976
nilshellerhoff <nils.hellerhoff@gmail.com>
2022-01-03 14:55:20 +0100

    moved decimal->hex logic

--- download_replay.py
+++ download_replay.py
@@ -8,6 +8,13 @@ from multiprocessing.pool import ThreadPool
 MAX_CONCURRENT_DOWNLOADS = 5
 
 def downloadReplay(replay_id, store_path=os.getcwd()):
+    """
+    download a replay from the replay server given it's id
+    """
+
+    # get the hex id in correct format
+    replay_id = _get_hex_id(replay_id)
+
     # create the folder
     os.mkdir(replay_id)
 
@@ -28,12 +35,10 @@ def downloadReplay(replay_id, store_path=os.getcwd()):
     return index
 
 
-def main():
-    try:
-        replay_id = sys.argv[1]
-    except:
-        print("Usage: download_replay.py <replay_id>")
-        return
+def _get_hex_id(replay_id):
+    """
+    from a replay id (64-bit int) get the appropriate hex notation (zero-padded 16 char int) regardless of decimal or hex input
+    """
 
     # replay_id is 64bit int (can be decimal or hex)
     # check length to find out, hex should be 16 chars (64 bit)
@@ -45,7 +50,19 @@ def main():
     # convert to hex
     replay_id_hex = "{:016x}".format(replay_id)
 
-    num_parts = downloadReplay(replay_id_hex)
+    return replay_id_hex
+
+
+def main():
+    try:
+        replay_id = sys.argv[1]
+    except:
+        print("Usage: download_replay.py <replay_id>")
+        return
+
+    num_parts = downloadReplay(replay_id)
+
+    replay_id_hex = _get_hex_id(replay_id)
 
     print(f"downloaded {num_parts}-part replay to {replay_id_hex}")
 
